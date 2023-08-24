2d41b5247eaad83452d79dfc9aacfc49776cdd64
Mahmood Ahmed <Mahmood.Ahmed.SE@outlook.com>
2023-08-24 14:24:06 +0400

    Fixing the pointPosition algo

--- main.cpp
+++ main.cpp
@@ -29,20 +29,28 @@ void getPosAndLines(fstream *mazeFileStream, vector<string> *lines, int **points
     int lineIndex = 0;
     string tempLine;
 
+     
+    /*
+    Adding string line by string line into "lines" vector pointer. 
+    Simealtaniously, scanning for start and end points of the maze into the vector 
+    pointer's pointer "pointsPosition" 
+    */
     while (getline(*mazeFileStream, tempLine))
     {
         lines->push_back(tempLine);
         
-
-        for(int index = 0; index < tempLine.length() && lineIndex < 2; index++)
+        if(regex_search(tempLine, pointRecoginationPattern))
         {
-            if(regex_search(string(1, tempLine[index]), pointRecoginationPattern))
+            for(int index = 0; index < tempLine.length() && pointIndex < 2; index++)
             {
-                pointsPosition[pointIndex][0] = lineIndex;
-                pointsPosition[pointIndex][1] = index;
-                pointIndex++;
+                if(regex_search(string(1, tempLine[index]), pointRecoginationPattern))
+                {
+                    pointsPosition[pointIndex][0] = lineIndex;
+                    pointsPosition[pointIndex][1] = index;
+                    pointIndex++;
+                }
             }
-        }
+        };
         
 
         lineIndex++;
@@ -54,16 +62,23 @@ int main(void)
     // initiating and opening maze text file.
     fstream *mazeFileStream = new fstream;
     (*mazeFileStream).open("./example.txt");
-
+    
     vector<string> *lines = new vector<string>;
+
+    /*
+        Maze start point line index is at [0][0] and its line character index is at [0][1].
+        Maze end point line index is at [1][0] and its line character index is at [1][1].
+    */ 
     int** pointsPosition = create2DArray(2, 2);
 
     getPosAndLines(mazeFileStream, lines, pointsPosition);
     
-    cout << pointsPosition[0][0];
-    cout << pointsPosition[0][1] << endl;
-    cout << pointsPosition[1][0];
-    cout << pointsPosition[1][1] << endl;
+
+
+
+    // cout << (*lines)[pointsPosition[0][0]][pointsPosition[0][1]] << endl;
+    // cout << (*lines)[pointsPosition[1][0]][pointsPosition[1][1]];
+
 
 
 
